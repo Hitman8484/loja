02d98875a890ff31e87cb72c67891ddcea332891
Hitman84 <matheusccc98@gmail.com>
2022-06-16 13:39:03 -0300

    dotContainer  7:40

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,12 +1,45 @@
-import { View, Text, } from "react-native";
+import { View, Text, Image, ScrollView, TouchableOpacity, StyleSheet } from "react-native";
 
-export default function Detail(){
+export default function Detail({ navigation }) {
+
+    navigation.setOptions({
+        headerTitle: 'Nike Moonlight'
+    })
+
     return (
-        <View>
-            <Text>
-                 Pagina Detalhes
-            </Text>
-        </View>
-    );
+        <ScrollView style={styles.container}>
+            <Image
+                source={require('../../assets/5.webp')}
+                style={styles.image}
+                resizeMode='cover'
+            />
 
+            <View>
+
+                <View>
+                    <Text style={[styles.title, { fontSize: 24 }]}>R$ 149,90</Text>
+                </View>
+                <View opacity={0.4}>
+                    <Text style={[styles.title, { fontSize: 30 }]}>Nike Moonlight</Text>
+                </View>
+
+            </View>
+
+        </ScrollView>
+    );
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width:'100%',
+        backgroundColor: '#FFF'
+    },
+
+    image: {
+        width: '100%'
+    },
+    title:{
+        fontFamily: 'Anton_400Regular',
+        paddingHorizontal: '3%',
+    }
+})
